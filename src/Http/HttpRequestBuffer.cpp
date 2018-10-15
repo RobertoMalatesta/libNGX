74d63c9d1a29005fce6be1c07cd4158a349a9be3
stubborn <stubborn_p@outlook.com>
2018-10-16 00:08:26 +0800

    [review]: review http request parse code

--- src/Http/HttpRequestBuffer.cpp
+++ src/Http/HttpRequestBuffer.cpp
@@ -85,7 +85,6 @@ HttpError HttpRequestBuffer::ProcessHttpRequest() {
 HttpError HttpRequestBuffer::ParseRequestLine() {
 
     u_char C, C1, C2, C3, C4, C5, C6, C7, C8;
-    BufferCursor TempCursor = ReadCursor;
 
     while (ReadByte(ReadCursor, 0, C)) {
        switch (RequestLineState) {
@@ -95,6 +94,8 @@ HttpError HttpRequestBuffer::ParseRequestLine() {
                if ((C<'A'|| C>'Z') && C != '_' && C != '-') {
                    return HttpError(EINVAL, "Invalid method!");
                }
+               RequestLineState = RL_Method;
+           case RL_Method:
                if (ReadBytes4(ReadCursor, 0, C1, C2, C3, C4)) {
                    if (C4 == ' ') {
                        if (C1 == 'G' && C2 == 'E' && C3 == 'T') {
@@ -129,41 +130,54 @@ HttpError HttpRequestBuffer::ParseRequestLine() {
                        }
                        ReadCursor = MoveCursor(ReadCursor, 5);
                    } else if (CmpByte(ReadCursor, 5, ' ')) {
-
-                       if (C1 == 'M' && C2 == 'K' && C3 == 'C' && C4 == 'O' && CmpByte(ReadCursor, 4, 'L')) {
+                       if (!ReadByte(ReadCursor, 4, C5)) {
+                           return HttpError(EFAULT, "Read failed!");
+                       }
+                       else if (C1 == 'M' && C2 == 'K' && C3 == 'C' && C4 == 'O' && C5 == 'L') {
                            Method = MKCOL;
                        }
-                       if (C1 == 'P' && C2 == 'A' && C3 == 'T' && C4 == 'C' && CmpByte(ReadCursor, 4, 'H')) {
+                       else if (C1 == 'P' && C2 == 'A' && C3 == 'T' && C4 == 'C' && C5 == 'H') {
                            Method = PATCH;
                        }
-                       if (C1 == 'T' && C2 == 'R' && C3 == 'A' && C4 == 'C' && CmpByte(ReadCursor, 4, 'E')) {
+                       else if (C1 == 'T' && C2 == 'R' && C3 == 'A' && C4 == 'C' && C5 == 'E') {
                            Method = TRACE;
                        } else {
                            return HttpError(EINVAL, "Invalid method!");
                        }
                        ReadCursor = MoveCursor(ReadCursor, 6);
                    } else if (CmpByte(ReadCursor, 6, ' ')) {
-                       if (C1 == 'D' && C2 == 'E' && C3 == 'L' && C4 == 'E' && CmpByte2(ReadCursor, 4, 'T', 'E')) {
+                       if (!ReadBytes2(ReadCursor, 4, C5, C6)) {
+                           return HttpError(EFAULT, "Read failed!");
+                       }
+                       else if (C1 == 'D' && C2 == 'E' && C3 == 'L' && C4 == 'E' && C5 == 'T' && C6 == 'E') {
                            Method = DELETE;
                        }
-                       if (C1 == 'U' && C2 == 'N' && C3 == 'L' && C4 == 'O' && CmpByte2(ReadCursor, 4, 'C', 'K')) {
+                       else if (C1 == 'U' && C2 == 'N' && C3 == 'L' && C4 == 'O' && C5 == 'C' && C6 == 'K') {
                            Method = UNLOCK;
+                       } else {
+                           return HttpError(EINVAL, "Invalid method!");
                        }
                    } else if(C1 == 'O' && C2 == 'P' && C3 == 'T' && C4 == 'I' && CmpByte4(ReadCursor, 4, 'O', 'N', 'S', ' ')) {
-                           Method = OPTIONS;
+                        Method = OPTIONS;
                    } else if (C1 == 'P' && C2 == 'R' && C3 == 'O' && C4 == 'P' &&
                         CmpByte4(ReadCursor, 4, 'F', 'I', 'N', 'D') && CmpByte(ReadCursor, 8, ' ')) {
-                           Method = PROPFIND;
+                        Method = PROPFIND;
                    } else if (C1 == 'P' && C2 == 'R' && C3 == 'O' && C4 == 'P' &&
-                          CmpByte4(ReadCursor, 4, 'P', 'A', 'T', 'C') && CmpByte2(ReadCursor, 8, 'H', ' ')) {
-                       Method = PROPPATCH;
+                        CmpByte4(ReadCursor, 4, 'P', 'A', 'T', 'C') && CmpByte2(ReadCursor, 8, 'H', ' ')) {
+                        Method = PROPPATCH;
                    }
                    else {
                        return HttpError(EINVAL, "Invalid method!");
                    }
                    ReadCursor = MoveCursor(ReadCursor, 7);
+                   RequestLineState = RL_Space_Before_Uri;
                }
-               RequestLineState = RL_Space_Before_Uri;
+               else {
+                   return HttpError(EFAULT, "Read failed!");
+               }
+               break;
+           case RL_Space_Before_Uri:
+
                break;
            default:
                printf("not handled!");
