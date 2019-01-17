e6cbbadabc5299e1c2c3be073b38374f1d92cd48
stubborn <stubborn_p@outlook.com>
2019-01-17 13:28:29 +0800

    [refactor]: remove useless const config

--- include/config_header.h
+++ include/config_header.h
@@ -5,8 +5,10 @@ namespace ngx {
             // Architecture Configuration
             const char CPUModule[] = "I386";
             const char Compiler[] = "Clang 6.0";
+
             const bool LITTLE_ENDING = true;
             const bool NON_ALIGNED = true;
+
             const size_t PAGE_SIZE = 4096;  // Byte(s)
             static uint32_t CACHELINE_SIZE = 64;    // Byte(s)
             const uint64_t TIME_RESOLUTION = 1000;  // us
@@ -18,13 +20,10 @@ namespace ngx {
             // BasicComponent Configuration
             const size_t BUFFER_MEMORY_BLOCK_SIZE = 1 * 128 * PAGE_SIZE;    //Byte(s)
             const size_t POOL_MEMORY_BLOCK_SIZE = 1 * 128 * PAGE_SIZE;      //Byte(s)
-            const int POOL_RESIDUAL = 16;
             const int POOL_RECYCLE_ROUND = 10000;
             const uint64_t DEFAULT_COLLECTOR_SIZE = 1024;
             const int THREAD_WAIT_TIME = 10 * 1000 * 1000;     // 10(ms)
             const uint32_t THREAD_GC_ROUND = 10000;
-            const uint32_t RECYCLE_GC_ROUND = 10000;
-            const uint32_t TIMER_PROCESS_COUNT = 500;
         }
     }
 }
