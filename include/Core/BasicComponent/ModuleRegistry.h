1c2cce113e1e419411c386a450dad738a21d3251
JiaqiGuo <jasperguo@outlook.com>
2019-01-22 15:44:54 +0800

    add module basic classes, will add http modules. eg:PreFilter, Mux, Service, PostFilter

---
+++ include/Core/BasicComponent/ModuleRegistry.h
@@ -0,0 +1,63 @@
+/**
+ *  @name Instance
+ *
+ *  @brief Abstract module instance type.
+ * */
+class Instance {
+
+protected:
+	/**Module name**/
+	const char *ModuleName;
+	/**Magic to identify module**/
+	uint32_t MajorMagic;
+	/**Magic to identify module detail**/
+	uint32_t MinorMagic;
+}
+
+/**
+ *  @name Module
+ *
+ *  @brief Module type.
+ * */
+class Moudle : DictionatyItem {
+
+protected:
+	/**Module name**/
+	const char *Name;
+	/**Module version**/
+	uint32_t Version;
+	/**Module author**/
+	const char *Author;
+	/**Module config manual**/
+	const char *Manual;
+
+public:
+	/**Create a instance of this module**/
+	RuntimeError Create(Instance &I) = 0;
+	/**Config a instance of this module**/
+	RuntimeError Config(Instance &I, Dictionaty &C) = 0;
+	/**Destory a instance of this module**/
+	RuntimeError Destroy(Instance &I) = 0;
+    /**If this instance create from this module**/
+	bool IsModuleInstance(Instance &I) = 0;
+}
+
+/**
+ *  @name ModuleRegistry
+ *
+ *  @brief Module Registry to manage modules.
+ * */
+class MoudleRegistry : Dictionaty {
+
+protected:
+	/**Working directory to load dynamic load library**/
+	char *WorkingDirectory = nullptr;
+
+public:	
+	/**Register a known module type to this registry**/
+	RuntimeError RegistModule(Module &M);
+	/** Register a new dynamic load module to this registry**/
+	RuntimeError RegistDynamicModule(char *ModuleName);
+	/**Remove a module type from this registry**/
+	RuntimeError UnregistModule(char *ModuleName);
+}
